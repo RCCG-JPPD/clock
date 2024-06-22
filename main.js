3451e3f385b713e6fc4c7b216d0aa90a55b011bd
Jide <JideOyelayo1@Gmail.com>
2024-06-22 10:56:11 +0100

    updated main.js to load both clocks, just need to add both clock positions

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow() {
-    // Create the browser window.
-    let win = new BrowserWindow({
+function createWindows() {
+    let privateWindow = new BrowserWindow({
         width: 250,
         height: 100,
+        x: 0,
+        y: 0,
         frame: false, 
         alwaysOnTop: true, 
         webPreferences: {
@@ -13,7 +14,22 @@ function createWindow() {
         },
     });
 
-    win.loadFile(path.join(__dirname, 'index.html'));
+    let publicWindow = new BrowserWindow({
+        width: 250,
+        height: 100,
+        x: 250,
+        y: 250,
+        frame: false, 
+        alwaysOnTop: true, 
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    publicWindow.loadFile(path.join(__dirname, 'public-clock.html'));
+
+    privateWindow.loadFile(path.join(__dirname, 'private-clock.html'));
 }
 
-app.on('ready', createWindow);
+
+app.on('ready', createWindows);
